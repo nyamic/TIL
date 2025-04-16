ff10663832772f0fdf907817c5e64f1f1f296bfc
nyamic <kimh@ewha.ac.kr>
2025-04-16 22:50:01 +0900

    ~#11022 25.04.16

--- basic/basic.cpp
+++ basic/basic.cpp
@@ -223,3 +223,385 @@
 
 //     return 0;
 // }
+
+
+
+
+// // #2480
+// #include <iostream>
+// int main()
+// {
+// 	using namespace std;
+
+// 	int dice1, dice2, dice3;
+// 	int prize = 0;
+// 	cin >> dice1 >> dice2 >> dice3;
+
+// 	if ((dice1 == dice2) && (dice1 == dice3))
+// 	{
+// 		prize = 10000 + dice1 * 1000;
+// 	}
+// 	else if ((dice1 == dice2) || (dice2 == dice3) || (dice1 == dice3))
+// 	{
+// 		prize = (dice2 == dice3) ? (1000 + dice2 * 100) : (1000 + dice1 * 100);
+// 	}
+// 	else
+// 	{
+// 		if (dice1 > dice2)
+// 		{
+// 			prize = (dice1 > dice3) ? dice1*100 : dice3*100;
+// 		}
+// 		else
+// 		{
+// 			prize = (dice2 > dice3) ? dice2 * 100 : dice3 * 100;
+// 		}
+// 	}
+
+// 	cout << prize;
+
+// 	return 0;
+// }
+
+
+
+
+
+
+
+
+// // #2884
+// #include <iostream>
+// int main() {
+// 	using namespace std;
+
+// 	int minutes, hours;
+// 	cin >> hours >> minutes;
+
+//     if (minutes >= 45)
+//     {
+//         minutes -= 45;
+//     }
+//     else
+//     {
+//         hours = (hours == 0) ? 23 : hours-1;
+//         minutes = 60 - (45 - minutes);
+//     }
+
+//     cout << hours << " " << minutes;
+
+// 	return 0;
+// }
+
+
+
+
+
+
+// // #2525
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int hours, minutes, cookingTime;
+//     cin >> hours >> minutes;
+//     cin >> cookingTime;
+
+//     minutes += cookingTime;
+    
+//     while(minutes >= 60)
+//     {
+//         hours = (hours == 23) ? 0 : hours+1;
+//             minutes -= 60;
+//     }
+
+//     cout << hours << " " << minutes;
+
+//     return 0;
+// }
+
+
+
+
+
+
+// // #14681
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int x, y, QuadrantN;
+//     cin >> x >> y;
+
+
+//     if(x > 0)
+//     {
+//         QuadrantN = (y > 0) ? 1 : 4; 
+//     }else
+//     {
+//         QuadrantN = (y > 0) ? 2 : 3; 
+//     }
+
+//     cout << QuadrantN;
+
+
+//     return 0;
+// }
+
+
+
+
+// // #2438
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int n;
+//     cin >> n;
+
+
+//     for(int i = 1; i<=n; i++)
+//     {
+//         for(int j = 1; j<=i; j++)
+//         {
+//            cout << "*";
+//         }
+        
+//         cout << endl;
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+
+
+// // #25304
+// #include <iostream>
+// int main(){
+//     using namespace std;
+
+//     int receiptTotalPrice, numberOfItemsType;
+//     cin >> receiptTotalPrice;
+//     cin >> numberOfItemsType;
+
+//     int priceArray[numberOfItemsType];
+//     int numberArray[numberOfItemsType];
+//     int i = 0;
+//     while(i < numberOfItemsType)
+//     {
+//         cin >> priceArray[i] >> numberArray[i];
+//         ++i;
+//     }
+
+//     int totalPrice = 0;
+//     for(i = 0; i < numberOfItemsType; ++i)
+//     {
+//         totalPrice += priceArray[i] * numberArray[i];
+//     }
+
+//     (receiptTotalPrice == totalPrice) ? cout<<"Yes" : cout<<"No";
+
+//     return 0;
+// }
+
+
+
+
+// // #27323
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int a, b;
+//     cin >> a >> b;
+//     cout << a*b;
+
+//     return 0;
+// }
+
+
+
+
+
+// #9063
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int dotsNumber;
+//     cin >> dotsNumber;
+
+//     int minX, maxX, minY, maxY;
+//     int x, y;
+//     cin >> x >> y;
+//     minX = x; maxX = x;
+//     minY = y; maxY = y;
+//     for(int i = 2; i <= dotsNumber; ++i)
+//     {
+//         cin >> x >> y;
+
+//         if(x < minX) minX=x;
+//         if(x > maxX) maxX=x;
+//         if(y < minY) minY=y;
+//         if(y > maxY) maxY=y;
+//     }
+
+//     cout << (maxX-minX) * (maxY-minY);
+
+//     return 0;
+// }
+
+
+
+
+
+// // #1085
+// #include <iostream>
+// int main()
+// {
+//     int x, y, w, h;
+//     std::cin >> x >> y >> w >> h;
+
+//     int min = (x <= y) ? x : y;
+//     if((w-x) < min) min=(w-x);
+//     if((h-y) < min) min=(h-y);
+
+//     std::cout << min;
+
+//     return 0;
+// }
+
+
+
+
+
+
+// // #2439
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int n;
+//     cin >> n;
+
+//     for(int i = 1; i <= n; ++i)
+//     {
+//         for(int j = n-i; j > 0; --j)
+//         {
+//             cout << " ";
+//         }
+
+//         for(int k = i; k > 0; --k)
+//         {
+//             cout << "*";
+//         }
+
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+// // #10950
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int repeatNumber;
+//     int a, b;
+//     cin >> repeatNumber;
+
+//     for(int i = 1; i <= repeatNumber; ++i)
+//     {
+//         cin >> a >> b;
+//         cout << a + b << "\n";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+// // #27866
+// #include <iostream>
+// #include <string>
+// int main()
+// {
+//     using namespace std;
+
+//     string word;
+//     cin >> word;
+
+//     int i;
+//     cin >> i;
+
+//     cout << word[i-1];
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// // #10952
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+//     int a = 1;
+//     int b = 1;
+
+//     while(a != 0)
+//     {
+//         cin >> a >> b;
+//         if(a == 0) break;
+//         cout << a + b << "\n";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+// // #11022
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+
+//     int a, b, t;
+//     cin >> t;
+
+//     for(int i = 1; i <= t; ++i)
+//     {
+//         cin >> a >> b;
+//         cout << "Case #" << i << ": " << a << " + " << b << " = " << a+b << "\n";
+//     }
+
+//     return 0;
+// }
+
+
+
+ 
+
+// 
