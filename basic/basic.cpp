b316cd6eb6f239d7eefed18c973aca5674f8d173
hyewon <kimh@ewha.ac.kr>
2025-11-13 19:55:15 +0900

    #10809 알파벳 찾기
    브2, 단계별로 풀어보기-문자열

--- basic/basic.cpp
+++ basic/basic.cpp
@@ -1,8 +1,7 @@
-// #2743 단어 길이 재기
+// #10809 알파벳 찾기
 
 /*
-    길이 재는 함수 뭐지
-    string.size() / string.length
+    배열 한 번에 같은 값으로 초기화하는 법
 */
 
 #include <iostream>
@@ -12,12 +11,29 @@ int main()
     using namespace std;
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     string s;
     cin >> s;
+    
+    int alph[26];
+    for (int i = 0; i < 26; i++)
+    {
+        alph[i] = -1;
+    }
+    
 
-    cout << s.length();
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (alph[s[i] - 'a'] == -1)
+        {
+            alph[s[i] - 'a'] = i;
+        }
+    }
 
+    for (int i = 0; i < 26; i++)
+    {
+        cout << alph[i] << " ";
+    }
 
     return 0;
 }
