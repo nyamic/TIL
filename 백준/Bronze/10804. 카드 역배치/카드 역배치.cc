a542865470d701369873e9002f1843d2b89d2fa1
Kim Hyewon <kimh@ewha.ac.kr>
2025-11-19 15:39:21 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -1,42 +1,32 @@
+//swap 쓰면 빠를 듯 
+
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int main()
-{
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int arr[20];
-    int copyArr[20];
 
-    // 배열 초기화
-    for (int i = 0; i < 20; i++)
-    {
+    for (int i = 0; i < 20; i++) {
         arr[i] = i + 1;
     }
 
-    // 역순 10번반복
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 20; j++)
-        {
-            copyArr[j] = arr[j];
-        }
-
+    for (int k = 0; k < 10; k++) {
         int a, b;
         cin >> a >> b;
 
-        for (int j = a-1; j < b; j++)
-        {
-            arr[j] = copyArr[a+b-2-j];
+        int l = a - 1;     // 시작 인덱스
+        int r = b - 1;     // 끝 인덱스
+        while (l < r) {
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
         }
-
-
     }
 
-    // 결과 출력
-    for (int i = 0; i < 20; i++)
-    {
+    for (int i = 0; i < 20; i++) {
         cout << arr[i] << " ";
     }
 
