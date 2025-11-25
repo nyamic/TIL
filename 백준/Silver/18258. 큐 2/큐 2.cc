c56a35ba54a1e6dc2517bf1638c2739d9d0b9f02
Kim Hyewon <kimh@ewha.ac.kr>
2025-11-26 00:37:16 +0900

    [Silver IV] Title: 큐 2, Time: 316 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+
+        if (s == "push")
+        {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if (s == "pop")
+        {
+            if (q.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << q.front() << "\n";
+                q.pop();
+            }
+        }
+        else if (s == "size")
+        {
+            cout << q.size() << "\n";
+        }
+        else if (s == "empty")
+        {
+            cout << q.empty() << "\n";
+        }
+        else if (s == "front")
+        {
+            if (q.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << q.front() << "\n";
+            }
+        }
+        else if (s == "back")
+        {
+            if (q.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << q.back() << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
